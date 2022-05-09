5cd2b7bb03b96245cf5db8b491d519149a8740c8
Hiram Chavez <virtual.liga@gmail.com>
2022-05-09 11:55:26 -0500

    Avances en sección Footer

--- source/components/footer.js
+++ source/components/footer.js
@@ -2,7 +2,7 @@
  * Construye el FOOTER de la página
  */
 
-const footer = document.querySelector('#footer');
-const p = document.createElement('p');
-p.textContent = 'El Footer: pié de página';
-footer.appendChild(p);
+// const footer = document.querySelector('#footer');
+// const p = document.createElement('p');
+// p.textContent = 'El Footer: pié de página';
+// footer.appendChild(p);
